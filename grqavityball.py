d7d0ccb37786efe5fab4e83fdbe628290d6b6ad4
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2024-11-24 10:02:16 +0100

    Update grqavityball.py

--- grqavityball.py
+++ grqavityball.py
@@ -8,7 +8,7 @@ b=random.randint(0,255)
 c=r,g,b
 grav=2000
 
-class circle():
+class circle1():
     def __init__(self,x,y):
         self.color=c
         self.cordinatex=x
@@ -19,12 +19,13 @@ class circle():
     def draw(self):
         pos=(self.cordinatex, self.cordinatey)
         screen.draw.filled_circle(pos,self.radius,c)
-obj1=circle(150,500)
+obj1=circle1(150,500)
 def draw():
     obj1.draw()
 def update(dt):
+    global obj1
     uy=obj1.vely
-    obj1.vy+=grav*dt
+    obj1.vely+=grav*dt
     obj1+=(uy+obj1.vely)*0.5*dt
     #detect and handle bounce
     if obj1.y>HEIGHT-obj1.radius:
