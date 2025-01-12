20ec98e4ac4a3e5e501931dbbe08f559b3b11daf
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-01-12 09:54:29 +0100

    Update bird.py

--- bird/bird.py
+++ bird/bird.py
@@ -9,13 +9,14 @@ pygame.display.set_caption("Flappy Bird")
 f1=pygame.font.SysFont("Arial",75)
 c1=(150,45,25)
 #variables
-fps=240
+fps=70
 gscroll=0
 scrolls=5
+fpipe=1000
 fly=False
+lastpipe=pygame.time.get_ticks()-fpipe
 gameo=False
 pipegap=100
-fpipe=1000
 score=0
 passp=False
 clock=pygame.time.Clock()
@@ -40,7 +41,7 @@ class bird(pygame.sprite.Sprite):
         self.clicked=False
     def update(self):
         if fly==True:
-            self.vel+=1.5
+            self.vel+=0.5
             if self.vel>10:
                 self.vel=10
             if self.rect.bottom<970:
@@ -51,7 +52,7 @@ class bird(pygame.sprite.Sprite):
                 self.clicked=True
                 self.vel-=10
             if pygame.mouse.get_pressed()[0]==0:
-                self.clicked=True
+                self.clicked=False
             flap=4
             self.counter+=1
             if self.counter>flap:
@@ -60,6 +61,21 @@ class bird(pygame.sprite.Sprite):
                 if self.index>=len(self.images):
                     self.index=0
                 self.image=self.images[self.index]
+class pipe(pygame.sprite.Sprite):
+    def __init__(self,x,y,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.image.load("img/pipe.png")
+        self.rect=self.image.get_rect()
+        if pos==1:
+            self.image=pygame.transform.flip(self.image,False,True)
+            self.rect.bottomleft=[x,y-int(pipegap/2)]
+        elif pos==-1:
+            self.rect.topleft=[x,y+int(pipegap/2)]
+    def update(self):
+        self.rect.x-=scrolls
+        if self.rect.right<0:
+            self.kill()
+pipegrp=pygame.sprite.Group()
 birdgrp=pygame.sprite.Group()
 objb=bird(100,100)
 birdgrp.add(objb)
@@ -68,11 +84,26 @@ while run:
     clock.tick(fps)
     s.blit(bg,(0,0))
     birdgrp.draw(s)
+    pipegrp.draw(s)
     birdgrp.update()
+    if fly==True and gameo==False:
+        timer=pygame.time.get_ticks()
+        if timer-lastpipe>fpipe:
+            pipeheigh=random.randint(-50,50)
+            bottompipe=pipe(1100,489+pipeheigh,-1)
+            toppipe=pipe(1100,489+pipeheigh,1)
+            pipegrp.add(bottompipe)
+            pipegrp.add(toppipe)
+            lastpipe=timer
+        pipegrp.update()
+        gscroll-=scrolls
+        if abs(gscroll)>40:
+            gscroll=0
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
         if event.type==pygame.MOUSEBUTTONDOWN and fly==False and gameo==False:
             fly=True
+    
     pygame.display.update()
 pygame.quit()
