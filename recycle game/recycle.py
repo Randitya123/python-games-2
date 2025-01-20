235374f5813b6af5940b8520a8a2c3fec9a285f4
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-01-20 19:58:52 +0100

    Update recycle.py

--- recycle game/recycle.py
+++ recycle game/recycle.py
@@ -16,14 +16,52 @@ class bin(pygame.sprite.Sprite):
         super().__init__()
         self.image=pygame.image.load("p1.png")
         self.rect=self.image.get_rect()
+class recyclable(pygame.sprite.Sprite):
+    def __init__(self,image):
+        super().__init__()
+        self.image=pygame.image.load(image)
+        self.rect=self.image.get_rect()
+class nonrecyclable(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image=pygame.image.load("plastic.png")
+        self.rect=self.image.get_rect()
+l1=["biobag.png","foood.png","paper.png"]
 obj1=bin()
 grp=pygame.sprite.Group()
+recyclablegrp=pygame.sprite.Group()
+nonrecyclablegrp=pygame.sprite.Group()
 grp.add(obj1)
-
+for i in range(25):
+    obj2=recyclable(random.choice(l1))
+    obj2.rect.x=random.randint(100,1000)
+    obj2.rect.y=random.randint(100,500)
+    grp.add(obj2)
+    recyclablegrp.add(obj2)
+for z in range(30):
+    obj3=nonrecyclable()
+    obj3.rect.x=random.randint(100,1000)
+    obj3.rect.y=random.randint(100,500)
+    grp.add(obj3)
+    nonrecyclablegrp.add(obj3)
 while run:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+    keypressed=pygame.key.get_pressed()
+    if keypressed[K_w]:
+        obj1.rect.y-=5
+    if keypressed[K_s]:
+        obj1.rect.y+=5
+    if keypressed[K_a]:
+        obj1.rect.x-=5
+    if keypressed[K_d]:
+        obj1.rect.x+=5
+    recyclecollide=pygame.sprite.spritecollide(obj1,recyclablegrp,True)
+    for i in recyclecollide:
+        score+=1
+        holdf=f1.render("Score:"+str(score),True,"black")
     s.blit(bg,(0,0))
     s.blit(holdf,(0,0))
     grp.draw(s)
